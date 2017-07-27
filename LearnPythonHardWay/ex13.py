aade8627292d03efd361918ccfb89585b8ad09a4
GrayLeo <haitao8@leju.com>
2017-07-27 08:22:51 +0800

    update

--- LearnPythonHardWay/ex13.py
+++ LearnPythonHardWay/ex13.py
@@ -3,7 +3,7 @@ from sys import argv
 
 script, first, second, third = argv
 
-print("The script is called:", script)
-print("Your first variable is:", first)
+print("The script is called: %s" % script)
+print("Your first variable is: " + first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
